d29fd232abd63c239ae7700a3ee0beeb9e68ed96
Pedro França <pfranca.neto@gmail.com>
2025-07-02 21:31:46 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import TrashIcon from './assets/icons/trash.svg?react';
+import Icon from './components/Icon';
+
 export default function App() {
-  return <div className="">Olá mundo!</div>;
+  return (
+    <div className="">
+      <div>
+        <Icon svg={TrashIcon} className="fill-green-base" />
+      </div>
+    </div>
+  );
 }
