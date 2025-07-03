44ba8bcc344471eb61813081b3e99a07da5d4ab2
Pedro França <pfranca.neto@gmail.com>
2025-07-02 22:22:28 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import TrashIcon from './assets/icons/trash.svg?react';
+import SpinnerIcon from './assets/icons/spinner.svg?react';
+
 import Icon from './components/Icon';
+import Badge from './components/Badge';
+import Text from './components/Text';
 
 export default function App() {
   return (
-    <div className="">
+    <div className="grid gap-3">
       <div>
-        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Text variant="body-md-bold">Olá miundo</Text>
+      </div>
+      <div>
+        <Icon svg={TrashIcon} />
+        <Icon svg={SpinnerIcon} animate />
+      </div>
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 3</Badge>
       </div>
     </div>
   );
