9ea5da476931881a5a133e8059a0341dc0a952e8
Pedro França <pfranca.neto@gmail.com>
2025-07-09 21:52:28 -0300

    criando componente Container

--- src/App.tsx
+++ src/App.tsx
@@ -10,38 +10,41 @@ import ButtonIcon from './components/ButtonIcon';
 import InputText from './components/InputText';
 import InputCheckBox from './components/InputCheckBox';
 import Card from './components/Card';
+import Container from './components/Container';
 
 export default function App() {
   return (
-    <div className="grid gap-3">
-      <div>
-        <Text variant="body-md-bold">Olá miundo</Text>
+    <Container>
+      <div className="grid gap-3">
+        <div>
+          <Text variant="body-md-bold">Olá miundo</Text>
+        </div>
+        <div>
+          <Icon svg={TrashIcon} />
+          <Icon svg={SpinnerIcon} animate />
+        </div>
+        <div>
+          <Badge variant="secondary">5</Badge>
+          <Badge variant="primary">2 de 3</Badge>
+        </div>
+        <div>
+          <Button icon={PlusIcon}>Deletar transação</Button>
+        </div>
+        <div>
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant="secondary" />
+          <ButtonIcon icon={TrashIcon} variant="tertiary" />
+        </div>
+        <div>
+          <InputText />
+        </div>
+        <div>
+          <InputCheckBox />
+        </div>
+        <div>
+          <Card size="md">Olá Muindo</Card>
+        </div>
       </div>
-      <div>
-        <Icon svg={TrashIcon} />
-        <Icon svg={SpinnerIcon} animate />
-      </div>
-      <div>
-        <Badge variant="secondary">5</Badge>
-        <Badge variant="primary">2 de 3</Badge>
-      </div>
-      <div>
-        <Button icon={PlusIcon}>Deletar transação</Button>
-      </div>
-      <div>
-        <ButtonIcon icon={TrashIcon} />
-        <ButtonIcon icon={TrashIcon} variant="secondary" />
-        <ButtonIcon icon={TrashIcon} variant="tertiary" />
-      </div>
-      <div>
-        <InputText />
-      </div>
-      <div>
-        <InputCheckBox />
-      </div>
-      <div>
-        <Card size="md">Olá Muindo</Card>
-      </div>
-    </div>
+    </Container>
   );
 }
