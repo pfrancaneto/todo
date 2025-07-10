3c7a5c15d356607ebcdf1445e99f4cc35132fcb0
Pedro França <pfranca.neto@gmail.com>
2025-07-10 15:17:23 -0300

    ajustando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,17 @@
-import TrashIcon from './assets/icons/trash.svg?react';
-import SpinnerIcon from './assets/icons/spinner.svg?react';
-import PlusIcon from './assets/icons/plus.svg?react';
-
-import Icon from './components/Icon';
-import Badge from './components/Badge';
-import Text from './components/Text';
-import Button from './components/Button';
-import ButtonIcon from './components/ButtonIcon';
-import InputText from './components/InputText';
-import InputCheckBox from './components/InputCheckBox';
-import Card from './components/Card';
-import Container from './components/Container';
-import Skeleton from './components/Skeleton';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Components from './pages/Components';
+import LayoutMain from './pages/LayoutMain';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <Container>
-      <div className="grid gap-3">
-        <div>
-          <Text variant="body-md-bold">Olá miundo</Text>
-        </div>
-        <div>
-          <Icon svg={TrashIcon} />
-          <Icon svg={SpinnerIcon} animate />
-        </div>
-        <div className="flex gap-1">
-          <Badge variant="secondary">5</Badge>
-          <Badge variant="primary">2 de 3</Badge>
-
-          <Badge loading>5</Badge>
-        </div>
-        <div>
-          <Button icon={PlusIcon}>Deletar transação</Button>
-        </div>
-        <div className="flex gap-1">
-          <ButtonIcon icon={TrashIcon} />
-          <ButtonIcon icon={TrashIcon} variant="secondary" />
-          <ButtonIcon icon={TrashIcon} variant="tertiary" />
-          <ButtonIcon icon={TrashIcon} loading />
-        </div>
-        <div>
-          <InputText />
-        </div>
-        <div>
-          <InputCheckBox />
-          <InputCheckBox loading />
-        </div>
-        <div>
-          <Card size="md">Olá Muindo</Card>
-        </div>
-
-        <div className="space-y-2">
-          <Skeleton className="h-6" />
-          <Skeleton className="h-6" />
-          <Skeleton className="w-96 h-6" />
-        </div>
-      </div>
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutMain />}>
+          <Route index element={<Home />} />
+          <Route path="/componentes" element={<Components />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
