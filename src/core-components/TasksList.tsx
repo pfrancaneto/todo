6bed73e34fe85388224945933d6f996d12247eb2
Pedro França <pfranca.neto@gmail.com>
2025-07-12 19:14:53 -0300

    criando hook para preprar a tarefa

--- src/core-components/TasksList.tsx
+++ src/core-components/TasksList.tsx
@@ -2,16 +2,22 @@ import Button from '../components/Button';
 import PlusIcon from '../assets/icons/plus.svg?react';
 import TaskItem from './TaskItem';
 import useTasks from '../hooks/useTasks';
+import useTask from '../hooks/useTask';
 
 export default function TasksList() {
   const { tasks } = useTasks();
+  const { prepareTask } = useTask();
 
   console.log(tasks);
+
+  function handleNewTask() {
+    prepareTask();
+  }
 
   return (
     <>
       <section>
-        <Button icon={PlusIcon} className="w-full">
+        <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
           Nova tarefa
         </Button>
       </section>
