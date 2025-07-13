9eed5b9565744e02d06a31902c49fd0430535c73
Pedro França <pfranca.neto@gmail.com>
2025-07-12 22:20:08 -0300

    atualizando o summary

--- src/core-components/TasksSummary.tsx
+++ src/core-components/TasksSummary.tsx
@@ -1,20 +1,24 @@
 import Badge from '../components/Badge';
 import Text from '../components/Text';
+import useTasks from '../hooks/useTasks';
 
 export default function TasksSummary() {
+  const { tasksCount, concludedTasksCount } = useTasks();
   return (
     <>
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="!text-gray-300">
           Tarefas criadas
         </Text>
-        <Badge variant="secondary">5</Badge>
+        <Badge variant="secondary">{tasksCount}</Badge>
       </div>
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="!text-gray-300">
           Concluídas
         </Text>
-        <Badge variant="primary">2 de 5</Badge>
+        <Badge variant="primary">
+          {concludedTasksCount} de {tasksCount}
+        </Badge>
       </div>
     </>
   );
